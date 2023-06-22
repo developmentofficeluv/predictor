aac369610b0cbf3339f79c069980f245f8490fc4
Sam Scully <samscully@Sams-MacBook-Pro.local>
2023-06-22 11:54:37 -0500

    fix multi bug

--- lib/predictor/base.rb
+++ lib/predictor/base.rb
@@ -222,7 +222,7 @@ module Predictor::Base
           item_keys = Predictor.redis.smembers(k).map { |set| matrix.redis_key(:items, set) }
 
           counts = Predictor.redis.multi do |multi|
-            item_keys.each { |key| Predictor.redis.scard(key) }
+            item_keys.each { |key| multi.scard(key) }
           end
 
           item_keys.zip(counts).each do |key, count|
